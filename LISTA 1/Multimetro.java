bd638e046ce17e6afee0d2592c87e141519c3d5a
Pedro Elias <pedromattar42@gmail.com>
2023-03-23 08:27:56 -0300

    questão multímetro

---
+++ LISTA 1/Multimetro.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Multimetro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int tam = 10;
+        double[] medicoes = new double[10];
+        double desvioPadrao = 0, somaDvp = 0, media = 0;
+        int i = 0;
+
+        for (i = 0; i < tam; i++) {
+            System.out.println("Digite o valor da medicao " + (i + 1) + ": ");
+            medicoes[i] = scanner.nextFloat();
+            media += medicoes[i];
+        }
+
+        media /= tam;
+
+        for (i = 0; i < tam; i++) {
+            somaDvp += Math.pow(medicoes[i] - media, 2);
+        }
+
+        desvioPadrao = Math.sqrt(somaDvp / tam);
+
+        System.out.println("A media eh: " + media);
+        System.out.println("O desvio padrao eh: " + desvioPadrao);
+        if (desvioPadrao > (media * 0.1)) {
+            System.out.println("O multimetro esta com problemas e nao pode ser utilizado!");
+        } else {
+            System.out.println("O multimetro esta em estado normal de funcionamento!");
+        }
+    }
+}
