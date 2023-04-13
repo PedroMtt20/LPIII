c9a8fce0e5b35a30f92de1d86c76411539c24f2b
Pedro Elias <pedromattar42@gmail.com>
2023-04-12 21:46:25 -0300

    comentando código

--- LISTA 2/craps.py
+++ LISTA 2/craps.py
@@ -1,3 +1,4 @@
+#valida se os números dos dados estão entre 1  e 6
 def validacaoDados(dado1, dado2):
     if ((dado1 > 6 or dado1 < 1) or (dado2 > 6 or dado2 < 1)):
         print("Os dados só podem assumir valores de 1 a 6.")
@@ -13,14 +14,14 @@ def main():
         dado1 = int(input("Dado 1:"))
         dado2 = int(input("Dado 2:"))
         
-        pontoJogador = dado1 + dado2
-        entradaValida = validacaoDados(dado1, dado2)
+        pontoJogador = dado1 + dado2 #Calcula o ponto do jogador com a soma dos dados
+        entradaValida = validacaoDados(dado1, dado2) #Verifica se os dados são válidos
         
-    if(pontoJogador == 7 or pontoJogador == 11):
+    if(pontoJogador == 7 or pontoJogador == 11): #se a soma dos dados for 7 ou 11, o jogador ganhou
         print("Voce ganhou :)")
-    elif(pontoJogador == 2 or pontoJogador == 3 or pontoJogador == 12):
+    elif(pontoJogador == 2 or pontoJogador == 3 or pontoJogador == 12): 
         print("Voce perdeu :(")
-    else:
+    else: #se a soma dos dados for diferente de 2, 3, 7, 11 ou 12, entra no estágio 2
         estagio2 = True
         print("Ponto", pontoJogador)
         print("Entrando no estagio 2!")
@@ -34,10 +35,11 @@ def main():
             entradaValida = validacaoDados(dado1, dado2)
             
         numeroJogadas+=1
-        somaDados = dado1 + dado2
+        somaDados = dado1 + dado2 #Calcula a soma dos dados da jogada atual
         
         print("Jogada", numeroJogadas, ": ", somaDados)
         
+        #Verifica se o jogador ganhou ou perdeu na jogada atual
         if (somaDados == pontoJogador):
             print("Você ganhou :)")
             estagio2 = False
--- LISTA 2/multimetro.py
+++ LISTA 2/multimetro.py
@@ -1,24 +1,31 @@
+#Nome: Pedro Elias Santiago Mattar
+
 import math
 
 def main():
     tam = 10; desvioPadrao = 0; somaDvp = 0; media = 0.0; valor = 0.0
     medicoes = []
-
+    
+    #Lendo as medições e calculando a média
     for i in range(tam):
         valor = float(input("Digite o valor da medicao {}: ".format(i+1)))
         media+=valor
         medicoes.append(valor)
         
+    #calculando a média dos valores informados
     media/=tam
 
+    #Calculando a soma dos desvios quadrados e o desvio padrão
     for i in range(tam):
         somaDvp += pow(medicoes[i] - media, 2)
 
+    #calculando o desvio padrão
     desvioPadrao = math.sqrt(somaDvp / tam)
 
     print("A media eh: ", media)
     print("O desvio padrao eh: ", desvioPadrao)
 
+    #Imprimindo os resultados e verificando se o multimetro está funcionando corretamente
     if (desvioPadrao > (media * 0.1)) :
         print("O multimetro esta com problemas e nao pode ser utilizado!")
     else :
