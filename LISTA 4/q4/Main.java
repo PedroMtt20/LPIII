2ee6bbb8ae22eabb7391bc478b9b041a53f8ca1f
Pedro Elias <pedromattar42@gmail.com>
2023-04-26 20:22:00 -0300

    questão 4

---
+++ LISTA 4/q4/Main.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        Ponto2D ponto1 = new Ponto2D(1, 3);
+        Ponto2D ponto2 = new Ponto2D(4, 1);
+    
+        ponto1.imprimirPonto(); // imprime (1.0,3.0)
+        ponto2.imprimirPonto(); // imprime (4.0,1.0)
+    
+        System.out.println("Ponto 1 esta no eixo X ? " + ponto1.isEixoX()); // imprime false
+        System.out.println("Ponto 2 esta no eixo Y ? " + ponto2.isEixoY()); // imprime false
+        System.out.println("Ponto 1 esta na origem ? " + ponto1.isEixos()); // imprime false
+        System.out.println("Ponto 2 esta na origem ? " + ponto2.isEixos()); // imprime false
+    
+        System.out.println("O quadrante do ponto 1 eh " + ponto1.quadrante()); // imprime 1
+        System.out.println("O quadrante do ponto 2 eh " + ponto2.quadrante()); // imprime 2
+    
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println("A distancia entre os pontos eh (aproximadamente) " + df.format(ponto1.distancia(ponto2))); // imprime 3,61  
+    }
+}
---
+++ LISTA 4/q4/Ponto2D.java
@@ -0,0 +1,61 @@
+public class Ponto2D {
+    private double x;
+    private double y;
+
+    public Ponto2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public boolean isEixoX() {
+        return y == 0;
+    }
+
+    public boolean isEixoY() {
+        return x == 0;
+    }
+
+    public boolean isEixos() {
+        return x == 0 && y == 0;
+    }
+
+    public void imprimirPonto() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+
+    public int quadrante() {
+        if (x > 0 && y > 0) {
+            return 1;
+        } else if (x < 0 && y > 0) {
+            return 2;
+        } else if (x < 0 && y < 0) {
+            return 3;
+        } else if (x > 0 && y < 0) {
+            return 4;
+        } else {
+            return 0;
+        }
+    }
+
+    public double distancia(Ponto2D ponto) {
+        final double distancia = Math.sqrt(Math.pow(ponto.x - x, 2) + Math.pow(ponto.y - y, 2));
+
+        return distancia;
+    }
+}
